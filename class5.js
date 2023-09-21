8850135fa18f19b7b2463940e78cd78e73597539
Usman-Mug <143292226+Usman-Mug@users.noreply.github.com>
2023-09-21 05:24:57 -0700

    Add files via upload

---
+++ class5.js
@@ -0,0 +1,16 @@
+function abc() {
+    let std_Total_Marks= prompt("enter your tm")
+let std_Obtained_Marks= prompt("enter your om")
+let res = (std_Obtained_Marks / std_Total_Marks) *100
+alert("you have got it " + res + "%")
+document.write("you have got it " + res + "%")
+console.log("you have got it " + res + "%")
+}
+abc();
+abc();
+abc();
+abc();
+abc();
+abc();
+abc();
+abc();
